853187c63fb094d427e285ae3de624f473eb8965
Mariotaku Lee <mariotaku.lee@gmail.com>
2020-11-24 03:53:45 +0900

    Improved build config

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+set APP_META_DIR=$1
+
+echo $1
+
+if [ -f $APP_META_DIR/appinfo.json ]; then
+  echo 'Application metadata is not present'
+  exit 1
+fi
+
 if [ -z $ARCH ]; then
   echo 'ARCH is not set'
   exit 1
@@ -10,4 +19,5 @@ if [ -z $WEBOS_CLI_TV ]; then
   exit 1
 fi
 
+cp -f $APP_META_DIR/* pkg_$ARCH
 $WEBOS_CLI_TV/ares-package pkg_$ARCH
